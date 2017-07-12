1ae0b5487bd017d07d4ff6bb4947f23c5d80c3cd
Enrique Besumán <ebesuman@gmail.com>
2017-07-12 15:01:35 +0200

    Initial Commit

---
+++ MyFirstPyGame/SnakeGame.py
@@ -0,0 +1,27 @@
+# Snake Game !
+import pygame
+import sys
+import random
+import time
+
+check_errors = pygame.init()
+
+if check_errors[1] != 0:
+    print (" (!) Had {0} Initializing errors, exiting ...".format(check_errors[1]))
+    sys.exit(-1)
+else:
+    print ("(+) PyGame successfully initialized!")
+
+#Play surface
+playsurface = pygame.display.set_mode((720,460))
+pygame.display.set_caption("Snake game !")
+
+#Colors
+red = pygame.Color(255,0,0) #Game over
+green = pygame.Color(0,255,0) #Snake
+blue = pygame.Color(0,0,255)  # To use at your discretion
+black = pygame.Color(0,0,0) #score
+white = pygame.Color(255,255,255) # background
+brown = pygame.Color(165,42,42)  #food
+
+time.sleep(5)
